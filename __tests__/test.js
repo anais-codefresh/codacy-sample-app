77f27b97769225b1d7fbb009de00f4bee63c174b
anais-codefresh <anais.urlichs@codefresh.io>
2020-12-18 10:54:47 +0100

    Changes to tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const printName = require('../build/index');
+const printName = require('../src/index');
 
 test('make sure the first name plus the last name equal the full name', () => {
   expect(printName("Hanna", "Baum")).toBe("Hanna Baum");
